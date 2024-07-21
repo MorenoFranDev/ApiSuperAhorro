b70df2b6d759f4e2671f398fcfb293eea2c2a5c2
MorenoFranDev <moreno21fran@gmail.com>
2024-07-21 20:55:56 -0300

    add common login

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -5,3 +5,22 @@ export const loginGoogle = (req, res) => {
     console.log(token)
     res.json(token)
 }
+
+export const local_login = async (req, res) => {
+    const { email, password } = req.body
+    const userWithEmail = await User.findOne({ where: { email } }).catch(
+        (err) => {
+            console.log("Error: ", err);
+        }
+    );
+
+    if (!userWithEmail)
+        res.state(500).send("Error in user or email")
+
+    const verify = await verifyPassword(password, WithEmail.password)
+    if (verify)
+        res.state(500).send("Error in user or email")
+
+    const token = jwt.sign({ "fullName": default_user.fullName, "Emails": default_user.email, "Profle": default_user.profile }, SecretJWT);
+    res.json(token)
+}
--- src/router/auth.js
+++ src/router/auth.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { loginGoogle } from "../controller/auth.js";
+import { local_login, loginGoogle } from "../controller/auth.js";
 import passport from "passport";
 import("../middleware/google.js")
 passport.initialize()
 const router = Router();
 
-router.get('/google',passport.authenticate('auth-google', { scope: ['profile', 'email'] }))
+router.get('/google', passport.authenticate('auth-google', { scope: ['profile', 'email'] }))
 router.get('/google/redirect', loginGoogle);
-
+router.post('/login', passport.authenticate('local-login'),local_login)
 export default router;
 
