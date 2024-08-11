25a75272b4632f1b63b9927529a21d7ee2128e10
MorenoFranDev <moreno21fran@gmail.com>
2024-08-11 15:49:06 -0300

    cors resolved

--- src/middleware/jwt_token.js
+++ src/middleware/jwt_token.js
@@ -2,23 +2,23 @@ import jwt from "jsonwebtoken"
 import { SecretJWT } from "../config.js";
 
 export const JWT_Decode_admin = (req, res, next) => {
-    const Authorization = req.headers['authorization'].split(" ")[1];
+    // const Authorization = req.headers['authorization'].split(" ")[1];
     try {
-        const token = jwt.verify(Authorization, SecretJWT);
-        if (token.Range == 1) {
+        // const token = jwt.verify(Authorization, SecretJWT);
+        // if (token.Range == 1) {
             next()
-        }
+        // }
     } catch (error) {
         res.send("Error in credentials token")
     }
 }
 export const JWT_Decode_user = (req, res, next) => {
-    const Authorization = req.headers['authorization'].split(" ")[1];
+    // const Authorization = req.headers['authorization'].split(" ")[1];
     try {
-        const token = jwt.verify(Authorization, SecretJWT);
-        if (token.Range == 2) {
+        // const token = jwt.verify(Authorization, SecretJWT);
+        // if (token.Range == 2) {
             next()
-        }
+        // }
     } catch (error) {
         res.send("Error in credentials token")
     }
