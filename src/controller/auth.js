f8eaa52641d561a6abd33db7a1c764f74d9872bd
MorenoFranDev <moreno21fran@gmail.com>
2024-08-21 15:57:08 -0300

    mod: router google 11

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -5,8 +5,8 @@ import { encryptPass, verifyPassword } from "../middleware/bcrypt.js";
 
 
 export const loginGoogle = (req, res) => {
-    console.log(req.newuser)
-    res.json(req.newuser);
+    console.log(req.user)
+    res.json(req.user);
 };
 
 export const local_login = async (req, res) => {
