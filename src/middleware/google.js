f902eaa7bc4cd7f5e2fe00850ddb5a8609120151
MorenoFranDev <moreno21fran@gmail.com>
2024-08-21 17:40:42 -0300

    mod: router google 17

--- src/middleware/google.js
+++ src/middleware/google.js
@@ -20,7 +20,7 @@ passport.use("auth-google", new GoogleStrategy({
     profile: profile.photos[0].value,
     range: 2 //Range: 1 -> Admin, 2 -> Users, 3 -> Local
   }
-  const newuser = await User.findOrCreate({ where: { googleId: profile.id }, defaults: default_user })
+  const newuser = await User.findOrCreate({ where: { googleId: profile.id }, defaults: default_user, attributes: ["id","range","googleId","fullName","email","profile"] })
   if (!newuser) {
     return (null, false, { message: 'Error in google acount' })
   }
