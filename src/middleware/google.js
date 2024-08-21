61bd78a7582e098fdd410a49f7768454a6619099
MorenoFranDev <moreno21fran@gmail.com>
2024-08-21 18:23:39 -0300

    mod: router google 24

--- src/middleware/google.js
+++ src/middleware/google.js
@@ -24,7 +24,7 @@ passport.use("auth-google", new GoogleStrategy({
     return (null, false, { message: 'Error in google acount' })
   }
 
-  console.log(newuser)
+  console.log(newuser[0].User.dataValues)
 
 //   const user = {
 //     "fullName": req.user.fullName,
