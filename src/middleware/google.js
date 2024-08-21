84ed512d6cbc66d562426e39122949d5e705c318
MorenoFranDev <moreno21fran@gmail.com>
2024-08-21 15:20:15 -0300

    mod: router google 9

--- src/middleware/google.js
+++ src/middleware/google.js
@@ -24,7 +24,7 @@ passport.use("auth-google", new GoogleStrategy({
   if (!newuser) {
     return (null, false, { message: 'Error in google acount' })
   }
-  return done(null, default_user);
+  return cb(null, default_user);
 }));
 
 
